156013b9e2324d7fa4310359378ba0c65092456f
imwaleed265 <muhammadwaleed.iqbal@abacuscambridge.com>
2021-10-12 12:18:43 +0500

    echo "api_product: $api_product" echo "developer: $developer" echo "app: $app"

--- integration.sh
+++ integration.sh
@@ -4,13 +4,16 @@
 # base64encoded=$2
 echo "ORG: $ORG"
 echo "base64encoded: $base64encoded"
+echo "api_product: $api_product"
+echo "developer: $developer"
+echo "app: $app"
 
-client_id=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/$ORG/apiproducts/Cicd-Prod-Product?query=list&entity=keys")
+client_id=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/$ORG/apiproducts/$api_product?query=list&entity=keys")
 
 id=$(jq -r .[0] <<< "${client_id}" )
 echo "client_id at script: '$id'"
 
-client_secret=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/$ORG/developers/hr@api.com/apps/hrapp/keys/$id")
+client_secret=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/$ORG/developers/$developer/apps/$app/keys/$id")
 
 secret=$(jq -r .consumerSecret <<< "${client_secret}" )
 echo "client_secret at script: '$secret'"

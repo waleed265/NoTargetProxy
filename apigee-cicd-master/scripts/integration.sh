75cec8ca7ac7285c67f2098e837998b43cd7e5f2
imwaleed265 <muhammadwaleed.iqbal@abacuscambridge.com>
2021-10-14 16:32:07 +0500

    echo "bearer_token="$bearer$accessToken" >> $GITHUB_ENV

--- apigee-cicd-master/scripts/integration.sh
+++ apigee-cicd-master/scripts/integration.sh
@@ -24,5 +24,6 @@ newman run $GITHUB_WORKSPACE/apigee-cicd-master/test/integration/$NEWMAN_TARGET_
 
 accessToken=$(cat env.json | jq -r '.values[] | select(.key=="accessToken").value')
 #echo "accessToken at script: $accessToken"
-declare bearer="Bearer ";
+bearer="Bearer "
+echo "accessToken="$accessToken" >> $GITHUB_ENV
 echo "bearer_token="$bearer$accessToken" >> $GITHUB_ENV

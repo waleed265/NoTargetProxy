05f7cefc539b94cf4d861d18026f6f5708f79ed4
imwaleed265 <muhammadwaleed.iqbal@abacuscambridge.com>
2021-10-18 15:51:20 +0500

    -r htmlextra --reporter-htmlextra-export ./newman_report.html

--- apigee-cicd-master/scripts/integration.sh
+++ apigee-cicd-master/scripts/integration.sh
@@ -20,7 +20,7 @@ secret=$(jq -r .consumerSecret <<< "${client_secret}" )
 
 sudo npm install -g newman 
 sudo npm install -g newman-reporter-htmlextra
-newman run $GITHUB_WORKSPACE/apigee-cicd-master/test/integration/$NEWMAN_TARGET_URL --reporters cli,junit --reporter-junit-export junitReport.xml,-r htmlextra --reporter-htmlextra-export ./newman_report.html --env-var client_id=$id --env-var client_secret=$secret --export-environment env.json
+newman run $GITHUB_WORKSPACE/apigee-cicd-master/test/integration/$NEWMAN_TARGET_URL --reporters cli,junit --reporter-junit-export junitReport.xml -r htmlextra --reporter-htmlextra-export ./newman_report.html --env-var client_id=$id --env-var client_secret=$secret --export-environment env.json
 #cat env.json
 
 accessToken=$(cat env.json | jq -r '.values[] | select(.key=="accessToken").value')

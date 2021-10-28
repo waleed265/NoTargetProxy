44a213d9e3d8139f31e21e9e16891dfb65b9e3c9
imwaleed265 <muhammadwaleed.iqbal@abacuscambridge.com>
2021-10-28 19:25:58 +0500

    ss

--- apigee-cicd-master/scripts/revision.sh
+++ apigee-cicd-master/scripts/revision.sh
@@ -9,14 +9,14 @@ echo "ORG: $ORG"
 echo "base64encoded: $base64encoded"
 echo "ProxyName: $ProxyName"
 
-token_response=$(curl -s -X POST https://majid-al-futtaim-group.login.apigee.com/oauth/token /
--H "Content-Type: application/x-www-form-urlencoded;charset=utf-8" /
--H "accept: application/json;charset=utf-8" /
--H "authorization: Basic ZWRnZWNsaTplZGdlY2xpc2VjcmV0"/
+token_response=$(curl -s -X POST https://majid-al-futtaim-group.login.apigee.com/oauth/token
+-H "Content-Type: application/x-www-form-urlencoded;charset=utf-8" 
+-H "accept: application/json;charset=utf-8" 
+-H "authorization: Basic ZWRnZWNsaTplZGdlY2xpc2VjcmV0"
 -d "grant_type=password&username=apigee.cicduser1@maf.ae&password=cicduser$")
 
 accessToken=$(jq -r '.access_token' <<< "${token_response}")
-echo "SAML Access Token:$accessToken"
+echo "SAML Access Token: $accessToken"
 
 revision_info=$(curl -H "Authorization: Bearer $accessToken" "https://api.enterprise.apigee.com/v1/organizations/$ORG/environments/$ENV/apis/$ProxyName/deployments")
 

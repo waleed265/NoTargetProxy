c84582aca8e376a20216b172e53351c36ba5df9d
imwaleed265 <muhammadwaleed.iqbal@abacuscambridge.com>
2021-10-28 19:17:08 +0500

    a

--- apigee-cicd-master/scripts/revision.sh
+++ apigee-cicd-master/scripts/revision.sh
@@ -9,7 +9,16 @@ echo "ORG: $ORG"
 echo "base64encoded: $base64encoded"
 echo "ProxyName: $ProxyName"
 
-revision_info=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/$ORG/environments/$ENV/apis/$ProxyName/deployments")
+token_response=$(curl -s -X POST https://majid-al-futtaim-group.login.apigee.com/oauth/token /
+-H "Content-Type: application/x-www-form-urlencoded;charset=utf-8" /
+-H "accept: application/json;charset=utf-8" /
+-H "authorization: Basic ZWRnZWNsaTplZGdlY2xpc2VjcmV0"/
+-d "grant_type=password&username=apigee.cicduser1@maf.ae&password=cicduser$")
+
+accessToken=$(jq -r '.access_token' <<< "${token_response}")
+echo "SAML Access Token:$accessToken"
+
+revision_info=$(curl -H "Authorization: Bearer $accessToken" "https://api.enterprise.apigee.com/v1/organizations/$ORG/environments/$ENV/apis/$ProxyName/deployments")
 
 stable_revision_number=$(jq -r .revision[0].name <<< "${revision_info}" )  
 

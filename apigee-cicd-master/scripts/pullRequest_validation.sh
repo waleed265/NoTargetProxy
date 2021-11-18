042d1ad8b0a374aed2a5de7663d9e512ca0d0206
imwaleed265 <muhammadwaleed.iqbal@abacuscambridge.com>
2021-11-18 18:16:01 +0500

    adding pullrequest validation script file

---
+++ apigee-cicd-master/scripts/pullRequest_validation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "SOURCE_BRANCH_NAME:$SOURCE_BRANCH_NAME"
+echo "TARGET_BRANCH_NAME:$TARGET_BRANCH_NAME"
+        
+	if [[$TARGET_BRANCH_NAME -eq "uat" && $SOURCE_BRANCH_NAME -ne "dev"]]
+	then
+         {
+            echo "Cannot create pull request from $SOURCE_BRANCH_NAME to $TARGET_BRANCH_NAME"
+            exit 1
+          }
+    fi

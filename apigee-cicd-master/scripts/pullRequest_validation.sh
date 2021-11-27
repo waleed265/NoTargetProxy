05bea08862c13702298161c95f4acdcd145b4d2b
imwaleed265 <muhammadwaleed.iqbal@abacuscambridge.com>
2021-11-27 23:40:48 +0500

    a

--- apigee-cicd-master/scripts/pullRequest_validation.sh
+++ apigee-cicd-master/scripts/pullRequest_validation.sh
@@ -3,7 +3,14 @@
 echo "SOURCE_BRANCH_NAME:$SOURCE_BRANCH_NAME"
 echo "TARGET_BRANCH_NAME:$TARGET_BRANCH_NAME"
         
-	if [[ "${TARGET_BRANCH_NAME}" == "uat" && "${SOURCE_BRANCH_NAME}" != "dev" ]];
+	if [[ "${TARGET_BRANCH_NAME}" == "dev" && "${SOURCE_BRANCH_NAME}" != "sandbox" ]];
+	then
+         {
+            echo "Cannot create Pull Request from '$SOURCE_BRANCH_NAME' to '$TARGET_BRANCH_NAME' branch...!!!"
+            exit 1
+          }
+	
+	elif [[ "${TARGET_BRANCH_NAME}" == "uat" && "${SOURCE_BRANCH_NAME}" != "dev" ]];
 	then
          {
             echo "Cannot create Pull Request from '$SOURCE_BRANCH_NAME' to '$TARGET_BRANCH_NAME' branch...!!!"
